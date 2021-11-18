2b6a98de43ff68911c96acecf547de475560d837
bhaveshd17 <bhaveshdhake8@gmail.com>
2021-11-19 00:05:37 +0530

    v1

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'developmentmailcb@gmail.com'
+EMAIL_HOST_USER = 'bhaveshdhake4@gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = 'Admin@123'
+EMAIL_HOST_PASSWORD = 'ernackvfkncbrqos'
